e832b9710a004f98b42d9da19d6483b7dd92245b
x <x@gmail.com>
2024-08-26 20:14:19 +0800

    update ACR

--- AE_ACR/WHM/WHMRotationEntry.cs
+++ AE_ACR/WHM/WHMRotationEntry.cs
@@ -10,45 +10,45 @@ using AEAssist.CombatRoutine.View.JobView;
 namespace AE_ACR.WHM;
 
 #if DEBUG
-// public class WHMRotationEntry : IRotationEntry
-// {
-//     public string AuthorName { get; set; } = "44451516";
-//     
-//     public Rotation Build(string settingFolder)
-//     {
-//         var rot = new Rotation(SlotResolvers)
-//         {
-//             TargetJob = Jobs.WhiteMage,
-//             AcrType = AcrType.Both,
-//             MinLevel = 1,
-//             MaxLevel = 100,
-//             Description = "这个ACR没什么用，留空的"
-//         };
-//
-//         return rot;
-//     }
-//
-//     public IRotationUI GetRotationUI()
-//     {
-//         return new JobViewWindow(new JobViewSave(), () => { },"");
-//     }
-//
-//     
-//     private readonly List<SlotResolverData> SlotResolvers = new()
-//     {
-//
-//
-//     };
-//     
-//     public void OnDrawSetting()
-//     {
-//
-//     }
-//
-//
-//     public void Dispose()
-//     {
-//
-//     }
-// }
+public class WHMRotationEntry : IRotationEntry
+{
+    public string AuthorName { get; set; } = "44451516";
+    
+    public Rotation Build(string settingFolder)
+    {
+        var rot = new Rotation(SlotResolvers)
+        {
+            TargetJob = Jobs.WhiteMage,
+            AcrType = AcrType.Both,
+            MinLevel = 1,
+            MaxLevel = 100,
+            Description = "这个ACR没什么用，留空的"
+        };
+
+        return rot;
+    }
+
+    public IRotationUI GetRotationUI()
+    {
+        return new JobViewWindow(new JobViewSave(), () => { },"");
+    }
+
+    
+    private readonly List<SlotResolverData> SlotResolvers = new()
+    {
+
+
+    };
+    
+    public void OnDrawSetting()
+    {
+
+    }
+
+
+    public void Dispose()
+    {
+
+    }
+}
 #endif
